29e3de9383223baef2e595814f5354f9a3634d65
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-04-11 13:12:07 +0200

    css

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ navBarToggle.addEventListener('click', function () {
 
     mainNav.classList.toggle('active');
 });
+
+
+
+let current_page = document.getElementsByTagName("a")
+
+for (i = 0; i < current_page.length; i++) {
+    console.log(current_page[i].href)
+    var x = current_page[i].href.includes("#");
+    if(x){
+        current_page[i].classList.add("current-page");
+    }
+}
